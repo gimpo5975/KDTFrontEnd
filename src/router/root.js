007c0d99cfe3093d6b37d993f3f69bf4c0334217
Wonjung <gimpo5975@naver.com>
2024-04-24 11:12:36 +0900

    css에 tailwind 임포트

---
+++ src/router/root.js
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router-dom";
+//createBrowserRouter : 어떤 경로(path)에는 어떤 컴포넌트를 보여 줄 것인지를 결정하는 역할
+//                      경로의 추가는 파라미터로 전달되는 배열의 내용으로 결정
+
+const root = createBrowserRouter([
+
+]);
+
+export default root;
