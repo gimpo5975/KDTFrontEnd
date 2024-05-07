832f8599ff9b14dc37e7c00223225aefa7b1fcd3
Wonjung <gimpo5975@naver.com>
2024-05-07 15:44:50 +0900

    수정페이지 추가

--- src/router/todoRouter.js
+++ src/router/todoRouter.js
@@ -4,6 +4,7 @@ const Loading = <div>Loading...</div>
 const TodoList = lazy(()=>import("../pages/todo/ListPage"))
 const TodoRead = lazy(()=>import("../pages/todo/ReadPage"))
 const TodoAdd = lazy(()=>import("../pages/todo/AddPage"))
+const TodoModify = lazy(()=>import("../pages/todo/ModifyPage"))
 
 
 const todoRouter = ()=>{
@@ -23,6 +24,10 @@ const todoRouter = ()=>{
         {
             path: "add",
             element : <Suspense fallback={Loading}><TodoAdd/></Suspense>
+        },
+        {
+            path: "modify/:tno",
+            element : <Suspense fallback={Loading}><TodoModify/></Suspense>
         },
     ]
 }
