b87f0901d05cd35056a14914aee08683de0ae6bf
Wonjung <gimpo5975@naver.com>
2024-05-07 10:33:43 +0900

    코드스플릿, Suspense, lazy 설정

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,8 +1,10 @@
 import React from "react";
 
 function MainPage(){
+    return(
         <div className="text-3xl">
             <div>MainPage</div>
         </div>
+        )
 }
 export default MainPage;
