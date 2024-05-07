01182960802ed342e586eb52fb4c7b9b0d76d241
Wonjung <gimpo5975@naver.com>
2024-05-07 10:46:28 +0900

    리액트 코드 스니펫 설치

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 function AboutPage(){
     return(
     <div className="text-3xl">
         AboutPage
+        <div>
+            <Link to={"/about"}>About</Link>
+        </div>
     </div>
     )
 }
