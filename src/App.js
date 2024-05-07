57652f8fd973cb80b6641b6513dd4230aca19e9e
Wonjung <gimpo5975@naver.com>
2024-05-07 10:07:03 +0900

    router파일->root.js 생성

--- src/App.js
+++ src/App.js
@@ -1,21 +1,3 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App(props) {
-
-//   return (
-
-//     <div className="text-6xl text-red-600 font-bold underline text-center">
-     
-//      Hello react world!!;
-    
-//     </div>
-  
-
-//   );
-
-// }
-
 
 function App() {
   return (
