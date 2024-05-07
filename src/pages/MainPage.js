5255d3978903e66731bca24064c3dae6499af17f
Wonjung <gimpo5975@naver.com>
2024-05-07 10:19:27 +0900

    기본경로 루트 설정

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,6 +1,8 @@
+import React from "react";
 
 function MainPage(){
         <div className="text-3xl">
             <div>MainPage</div>
         </div>
 }
+export default MainPage;
--- src/router/root.js
+++ src/router/root.js
@@ -1,7 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainPage from "../pages/MainPage"
+import { Suspense, lazy } from "react";
+
+const Loading = <div>Loading...</div>
+const Main = lazy(()=> <MainPage/>)
 
 const root  =createBrowserRouter([
-
+    {
+        path : "",
+        element : <Suspense fallback={Loading}>Main</Suspense>
+    }
 ])
 
 export default root;
