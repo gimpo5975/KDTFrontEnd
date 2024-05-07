dda7efbb3282f4f886462929d0b90c22c660c379
Wonjung <gimpo5975@naver.com>
2024-05-07 10:35:13 +0900

    Link 설정

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function MainPage(){
     return(
         <div className="text-3xl">
+
+        <div>
+            <Link to={"/about"}>About</Link>
+        </div>
+
             <div>MainPage</div>
         </div>
         )
