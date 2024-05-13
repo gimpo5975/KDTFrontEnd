54c0705079f4c76463f116eeab489579f80877a4
Wonjung <gimpo5975@naver.com>
2024-05-13 16:27:06 +0900

    수정페이지 화면 개발

--- src/Components/todo/ModifyComponent.js
+++ src/Components/todo/ModifyComponent.js
@@ -19,13 +19,62 @@ const ModifyComponent = ({ tno }) => {
         });
     }, [tno]);
 
-    return ( 
-        <div className ="border-2 border-sky-200 mt-10 m-2 p-4">
+    const handleChangeTodo = (e) => {
+        console.log(e.target.name, e.target.value);
+        todo[e.target.name] = e.target.value;
+        setTodo({ ...todo });
+    };
+
+    const handleChangeTodoComplete = (e) => {
+        const value = e.target.value;
+        todo.complete = value === 'Y';
+        setTodo({ ...todo });
+    };
+
+    return (
+        <div className="border-2 border-sky-200 mt-10 m-2 p-4">
+            <div className="flex justify-center mt-10">
+                <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+                    <div className="w-1/5 p-6 text-right font-bold">TNO</div>
+                    <div className="w-4/5 p-6 rounded-r border border-solid shadow-md bg-gray-100">{todo.tno}</div>
+                </div>
+            </div>
+            <div className="flex justify-center">
+                <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+                    <div className="w-1/5 p-6 text-right font-bold">TITLE</div>
+                    <input className="w-4/5 p-6 rounded-r border border-solid border-neutral-300 shadow-md"
+                        name="title" type="text" value={todo.title} onChange={handleChangeTodo} />
+                </div>
+            </div>
+            <div className="flex justify-center">
+                <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+                    <div className="w-1/5 p-6 text-right font-bold">CONTENT</div>
+                    <input className="w-4/5 p-6 rounded-r border border-solid border-neutral-300 shadow-md"
+                        name="content" type="text" value={todo.content} onChange={handleChangeTodo} />
+                </div>
+            </div>
+            <div className="flex justify-center">
+                <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+                    <div className="w-1/5 p-6 text-right font-bold">DUEDATE</div>
+                    <input className="w-4/5 p-6 rounded-r border border-solid border-neutral-300 shadow-md"
+                        name="dueDate" type="date" value={todo.dueDate} onChange={handleChangeTodo} />
+                </div>
+            </div>
+            <div className="flex justify-center">
+                <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+                    <div className="w-1/5 p-6 text-right font-bold">COMPLETE</div>
+                    <select name="status" className="border-solid border-2 rounded m-1 p-2"
+                        onChange={handleChangeTodoComplete} value={todo.complete ? 'Y' : 'N'}>
+                        <option value='Y'>Completed</option>
+                        <option value='N'>Not Yet</option>
+                    </select>
+                </div>
+            </div>
             <div className="flex justify-end p-4">
-                <button type="button" className="inline-block rounded p-4 m-2 text-xl w-32 text-white bg-red-500">    
-                    Delete 
+                <button type="button" className="inline-block rounded p-4 m-2 text-xl w-32 text-white bg-red-500">
+                    Delete
                 </button>
-                <button type="button" className="rounded p-4 m-2 text-xl w-32 text-white bg-blue-500" >
+                <button type="button" className="rounded p-4 m-2 text-xl w-32 text-white bg-blue-500">
                     Modify
                 </button>
             </div>
