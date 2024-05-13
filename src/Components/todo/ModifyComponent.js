9ccf16e7d5b12591f0a0569e5d224141db7b8d7f
Wonjung <gimpo5975@naver.com>
2024-05-13 16:17:17 +0900

    수정페이지 get 방식으로 데이터 가져오기

--- src/Components/todo/ModifyComponent.js
+++ src/Components/todo/ModifyComponent.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getOne } from '../../api/todoApi';
 
-const ModifyComponent = () => {
-    return (
-        <div>
-            ModifyComponent
+const initState = {
+    tno: 0,
+    title: '',
+    content: '',
+    dueDate: '',
+    complete: false
+};
+
+const ModifyComponent = ({ tno }) => {
+    const [todo, setTodo] = useState(initState);
+
+    useEffect(() => {
+        getOne(tno).then(data => {
+            console.log(data);
+            setTodo(data);
+        });
+    }, [tno]);
+
+    return ( 
+        <div className ="border-2 border-sky-200 mt-10 m-2 p-4">
+            <div className="flex justify-end p-4">
+                <button type="button" className="inline-block rounded p-4 m-2 text-xl w-32 text-white bg-red-500">    
+                    Delete 
+                </button>
+                <button type="button" className="rounded p-4 m-2 text-xl w-32 text-white bg-blue-500" >
+                    Modify
+                </button>
+            </div>
         </div>
     );
 };
